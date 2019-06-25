21525de061c4d94999c28f7a8065aefbe20fe2c3
Alexandre <menigault.ipgp.fr>
2019-06-25 17:04:51 +0200

    Send colors of row plots

--- public/index.php
+++ public/index.php
@@ -1,28 +1,31 @@
 <?php
-require_once __DIR__.'/../config.php';
-require_once __DIR__.'/../src/functions.php';
-require_once __DIR__.'/../src/Entities/File.php';
+require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../src/functions.php';
+require_once __DIR__ . '/../src/Entities/File.php';
 
-route('GET', '^/$', function () {
-});
+route('GET', '^/$', function () { });
 
-route("GET", '^/api/data/(?<obs>.+)/(?<date>.+)/(?<type>.+)$', function($params) {
+route("GET", '^/api/data/(?<obs>.+)/(?<date>.+)/(?<type>.+)$', function ($params) {
     $file = new File($params["obs"], $params["type"], $params["date"]);
-    
+
     @ini_set('zlib.output_compression', 0);
     header(http_response_code(200));
     header("Content-Type: application/json");
-    
+
     $fh = fopen('php://output', 'w');
-    $i= 0;
+    $i = 0;
     fputs($fh, '[');
-    foreach($file->read() as $line) {
-        if($i == 0) {$line = array("header" => explode(",", $line), "type" => $file->type, "date"=> $file->date); $i++;}
-        fputs($fh, json_encode($line).',');
+    foreach ($file->read() as $line) {
+        if ($i == 0) {
+            $line = array("header" => explode(",", $line), "type" => $file->type, "date" => $file->date);
+            if($file->type == $file::TYPE_RAW ) $line["colors"] = ["#080", "#008b8b", "#ff8c00", "#9400d3", "#000"];
+            $i++;
+        }
+        fputs($fh, json_encode($line) . ',');
     }
-     // FIXME: find a way not to send an empty JSONObject, because of the trailing comma
-     // WORKAROUND: Remove the last object in the array client side
-    fputs($fh,'{}]');
+    // FIXME: find a way not to send an empty JSONObject, because of the trailing comma
+    // WORKAROUND: Remove the last object in the array client side
+    fputs($fh, '{}]');
     $st = ob_get_clean();
     exit($st);
 });
