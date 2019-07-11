ac5f22ff4fb0992ebd88eb422c0ad561ff809ea2
Alexandre <menigault.ipgp.fr>
2019-07-11 16:38:51 +0200

    Handle errors

--- src/Upload.php
+++ src/Upload.php
@@ -12,32 +12,46 @@ class Upload
     static function uploader()
     {
         $filename = $_FILES["fichier"]["name"];
-        $string = explode("-", $filename);
-        $type = explode(".", $string[1])[0];
-        list($code, $date) = Upload::parseFile($string[0]);
+        try {
+            $string = explode("-", $filename);
+            $type = explode(".", $string[1])[0];
 
-        $file = new File($code, $type, $date->getTimestamp());
-        $path = $file->getFilepath(true);
-        $dirname = pathinfo($path)["dirname"];
+            list($code, $date) = Upload::parseFile($string[0]);
 
-        // If directory doesn't exists, create it and all directories before it as well
-        if (!file_exists($dirname))
-            mkdir($dirname, 0777, true);
+            $file = new File($code, $type, $date->getTimestamp());
+            $path = $file->getFilepath(true);
+            $dirname = pathinfo($path)["dirname"];
 
-        $md5 = md5_file($_FILES['fichier']['tmp_name']); // Create hash from it
-        $imported = count(file($_FILES['fichier']['tmp_name'])) - 1; // Remove header in the line count
-        $is_uploaded = move_uploaded_file($_FILES['fichier']['tmp_name'], $path); // Move the uploaded file to its final location
+            // If directory doesn't exists, create it and all directories before it as well
+            if (!file_exists($dirname))
+                mkdir($dirname, 0777, true);
 
-        if ($is_uploaded) {
-            return array(
-                "saved" => true,
-                "md5" => $md5,
-                "size" => $_FILES['fichier']['size'],
-                "imported" => $imported
-            );
-        } else {
+            $md5 = md5_file($_FILES['fichier']['tmp_name']); // Create hash from it
+            $imported = count(file($_FILES['fichier']['tmp_name'])) - 1; // Remove header in the line count
+            $is_uploaded = move_uploaded_file($_FILES['fichier']['tmp_name'], $path); // Move the uploaded file to its final location
+
+            if ($is_uploaded) {
+                return array(
+                    "saved" => true,
+                    "md5" => $md5,
+                    "size" => $_FILES['fichier']['size'],
+                    "imported" => $imported
+                );
+            } else {
+                return array(
+                    "saved" => false,
+                );
+            }
+        } catch (Error $e) {
+            $error = new Error("Cannot parse filename " . $filename);
+            $errJson = json_encode(array(
+                "message" => $error->getMessage(),
+                "trace" => $error->getTrace(),
+            ));
+            error_log($errJson);
             return array(
                 "saved" => false,
+                "error" => $errJson
             );
         }
     }
